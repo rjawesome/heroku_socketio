b6d04dc2cfb5dd3dda799b862bfc490ce4f07bea
rjawesome <aonedtop@gmail.com>
2020-05-30 19:56:46 -0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, make_response, redirect
+from flask_socketio import SocketIO, send, emit
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@socketio.on("message")
+def handleMessage(data):
+    emit("new_message",data,broadcast=True)
+
+socketio.run(app, debug=True, port=5004)
