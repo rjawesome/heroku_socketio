83bfac92b75b6310251826f1f2918fd2dea63ea7
rjawesome <aonedtop@gmail.com>
2020-05-30 20:15:52 -0700

    Update app.py

--- app.py
+++ app.py
@@ -13,4 +13,4 @@ def index():
 def handleMessage(data):
     emit("new_message",data,broadcast=True)
 
-socketio.run(app, debug=True, port=5004)
+socketio.run(app, debug=True, port=int(os.environ.get('PORT', 5004)))
